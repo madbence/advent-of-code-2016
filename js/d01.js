7f657fb8134b30404298bd27f96588c5ce4a4474
Bence Dányi <bence@danyi.me>
2016-12-02 02:18:24 +0100

    day 01 part 2

--- js/d01.js
+++ js/d01.js
@@ -2,14 +2,24 @@ const fs = require('fs');
 const input = fs.readFileSync('./d01.txt', {
   encoding: 'utf-8',
 });
+const visits = {};
+const visit = (x, y) => visits[`${x}-${y}`] = 1;
+const visited = (x, y) => visits[`${x}-${y}`];
 
 const result = input.split(', ').reduce((state, [dir, ...steps]) => {
+  const {x, y} = state;
   const next = (4 + state.dir + (dir == 'R' ? 1 : -1)) % 4;
-  return {
-    dir: next,
-    x: state.x + (next % 2 == 1 ? parseInt(steps.join(''), 10) * (2 - next) : 0),
-    y: state.y + (next % 2 == 0 ? parseInt(steps.join(''), 10) * (1 - next) : 0),
-  };
+  steps = parseInt(steps.join(''));
+  for (let i = 0; i < steps; i++) {
+    if (visited(state.x, state.y)) console.log(Math.abs(state.x) + Math.abs(state.y));
+    visit(state.x, state.y);
+    state = {
+      dir: next,
+      x: state.x + (next % 2 == 1 && 1 * (2 - next)),
+      y: state.y + (next % 2 == 0 && 1 * (1 - next)),
+    };
+  }
+  return state;
 }, { dir: 0, x: 0, y: 0 });
 
 console.log(Math.abs(result.x) + Math.abs(result.y));
